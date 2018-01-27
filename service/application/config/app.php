56723ca6204549bb0c9718ac3e375f49e65f84f8
siriusSupreme <guoplc@163.com>
2018-01-27 21:13:09 +0800

    ;;

---
+++ service/application/config/app.php
@@ -0,0 +1,15 @@
+<?php
+
+  return [
+    // 必须在应用中配置，模块配置无效
+    'app_debug'        => false,
+    'app_trace'        => false,
+    'class_suffix'     => false,
+    'default_filter'   => '',
+    'root_namespace'   => '',
+    'pathinfo_depr'    => '/',
+    'url_route_must'   => false,
+    'auto_bind_module' => false,
+    'default_lang'     => 'zh-cn',
+    'lang_switch_on'   => true,
+  ];
