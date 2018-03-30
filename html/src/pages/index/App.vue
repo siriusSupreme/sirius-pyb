faa1e2f2d58cfa908f966e6608412ce1aa59a6ed
siriusSupreme <guoplc@163.com>
2018-03-30 15:26:45 +0800

    ''

--- html/src/pages/index/App.vue
+++ html/src/pages/index/App.vue
@@ -1,14 +1,36 @@
 <template >
   <div class="container" >
+    <span @click="demo">demo</span>
     Hello World
   </div >
 </template >
 
 <script >
+import toastr from 'toastr'
+
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      msg: 'hello word'
+    }
+  },
+  mounted () {
+    console.log(toastr)
+  },
+  methods: {
+    demo (e) {
+      console.log(e)
+    }
+  }
 }
 </script >
+
+<style lang="stylus" >
+  .container{
+    background-color: red
+  }
+</style >
 
 <style lang="stylus" scoped >
   .container{
