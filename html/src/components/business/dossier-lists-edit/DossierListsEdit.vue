02daf4d533115e5954d6cd226d4e392aee3f9198
siriusSupreme <guoplc@163.com>
2018-05-02 16:35:26 +0800

    ''

--- html/src/components/business/dossier-lists-edit/DossierListsEdit.vue
+++ html/src/components/business/dossier-lists-edit/DossierListsEdit.vue
@@ -1,13 +1,63 @@
 <template>
   <dialog-container :title="案件基本信息">
-    <form class="form-horizontal" >
-      <div class="form-group form-group-sm">
-        <label class="control-label col-sm-4" >案卷编号：</label >
-        <div class="col-sm-8">
-          <input type="text" class="form-control" name="caseNo" />
+    <div class="dsw-form-wrapper">
+      <form class="form-horizontal" >
+        <div class="form-group form-group-sm">
+          <label class="control-label col-sm-3 dsw-required" >案卷编号：</label >
+          <div class="col-sm-9">
+            <input type="text" class="form-control" name="caseNo" />
+          </div>
         </div>
-      </div>
-    </form >
+        <div class="form-group form-group-sm">
+          <label class="control-label col-sm-3 dsw-required" >案卷名称：</label >
+          <div class="col-sm-9">
+            <input type="text" class="form-control" name="caseNo" />
+          </div>
+        </div>
+        <div class="form-group form-group-sm">
+          <label class="control-label col-sm-3 dsw-required" >案卷类型：</label >
+          <div class="col-sm-9">
+            <input type="text" class="form-control" name="caseNo" />
+          </div>
+        </div>
+        <div class="form-group form-group-sm">
+          <label class="control-label col-sm-3 dsw-required" >案卷状态：</label >
+          <div class="col-sm-9">
+            <input type="text" class="form-control" name="caseNo" />
+          </div>
+        </div>
+        <div class="form-group form-group-sm">
+          <label class="control-label col-sm-3 dsw-required" >立案时间：</label >
+          <div class="col-sm-9">
+            <input type="text" class="form-control" name="caseNo" />
+          </div>
+        </div>
+        <div class="form-group form-group-sm">
+          <label class="control-label col-sm-3 dsw-required" >简要案情：</label >
+          <div class="col-sm-9">
+            <textarea name="" id="" cols="30" rows="10" ></textarea >
+          </div>
+        </div>
+        <div class="form-group form-group-sm">
+          <label class="control-label col-sm-3 dsw-required" >主办民警：</label >
+          <div class="col-sm-9">
+            <input type="text" class="form-control" name="caseNo" />
+          </div>
+        </div>
+        <div class="form-group form-group-sm">
+          <label class="control-label col-sm-3 dsw-required" >协办民警：</label >
+          <div class="col-sm-9">
+            <input type="text" class="form-control" name="caseNo" />
+          </div>
+        </div>
+        <div class="form-group form-group-sm">
+          <label class="control-label col-sm-3 dsw-required" >备注：</label >
+          <div class="col-sm-9">
+            <textarea name="" id="" cols="30" rows="10" ></textarea >
+          </div>
+        </div>
+      </form >
+    </div>
   </dialog-container>
 </template>
 
@@ -30,5 +80,8 @@ export default {
 </style>
 
 <style lang="stylus" scoped>
-
+.dsw-form-wrapper{
+  width :60%;
+  margin :0 auto;
+}
 </style>
